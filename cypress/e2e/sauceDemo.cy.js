189ae8be79a5346b9eb1281a18c11def96b79d3c
yosoymath <matheusmat170@gmail.com>
2024-07-28 14:17:12 -0300

    Adaptando código e adicionando caso de teste para Fluxo de finalização de compra.

--- cypress/e2e/sauceDemo.cy.js
+++ cypress/e2e/sauceDemo.cy.js
@@ -17,10 +17,15 @@ function adicionarCarrinho() {
     cy.get(':nth-child(2) > .pricebar > .btn_primary').click()
     cy.get(':nth-child(4) > .pricebar > .btn_primary').click()
 }
-function removerProdutos(){
-    cy.get('.pricebar > .btn_secondary').each(($el) =>{ // Itera sobre cada elemento encontrado.
+function removerProdutos() {
+    cy.get('.pricebar > .btn_secondary').each(($el) => { // Itera sobre cada elemento encontrado.
         cy.wrap($el).click();   // Envolve o elemento jQuery em um objeto Cypress e clica nele.
     })
+}
+function preencheDadosCheckout (){
+    cy.get(SauceDemoElements.nome()).type('Teste');
+    cy.get(SauceDemoElements.sobrenome()).type('Teste 2');
+    cy.get(SauceDemoElements.CEP()).type('58693256');
 }
 
 describe('Cenários Sauce Demo', () => {
@@ -44,8 +49,8 @@ describe('Cenários Sauce Demo', () => {
         imagemCatalogo();
     })
 
-    it('Logando com usuário com problema, e verificando se existe foto', () => { 
-    //Teste irá dar erro, porque a validação é para saber se tem foto, como com esse usuário não tem, então teste não irá passar.
+    it('Logando com usuário com problema, e verificando se existe foto', () => {
+        //Teste irá dar erro, porque a validação é para saber se tem foto, como com esse usuário não tem, então teste não irá passar.
 
         loginPage.loginProblemUser();
         imagemCatalogo();
@@ -59,19 +64,34 @@ describe('Cenários Sauce Demo', () => {
         adicionarCarrinho();
 
         //Assert para saber se os produtos foram adicionados
-        cy.get('.fa-layers-counter').should('be.visible');  
+        cy.get(SauceDemoElements.contadorPrd()).should('be.visible');
     })
-    it('Remover produtos do carrinho', () =>{
-        //Função de login
-        loginPage.loginStandardUser();
 
-        //Função para adicionar produtos ao carrinho
+    it('Remover produtos do carrinho', () => {
+        loginPage.loginStandardUser();
         adicionarCarrinho();
 
         //Função para remover produtos do carrinho
         removerProdutos();
 
         //Assert para saber não existe o elemento de contador no carrinho
-        cy.get('.fa-layers-counter').should('not.exist');  
+        cy.get(SauceDemoElements.contadorPrd()).should('not.exist');
+    })
+    
+    it.only('Fluxo completo de finalização de compra', () =>{
+        loginPage.loginStandardUser();
+        adicionarCarrinho();
+
+        cy.get(SauceDemoElements.contadorPrd()).click();
+        cy.get(SauceDemoElements.btnCheckoutFinish()).click();
+
+        preencheDadosCheckout();
+
+        cy.get(SauceDemoElements.btnContinue()).click();
+
+        cy.get(SauceDemoElements.qtdCarrinho()).should('be.visible')
+        cy.get(SauceDemoElements.btnCheckoutFinish()).click();
+
+        cy.contains('THANK YOU FOR YOUR ORDER').should('be.visible')
     })
 })
--- cypress/e2e/sauceDemo.js
+++ cypress/e2e/sauceDemo.js
@@ -1,12 +1,33 @@
 class sauceDemoElements{
     foto = () => {
-        return "#item_1_img_link > .inventory_item_img";
+        return "#item_1_img_link > .inventory_item_img"
     }
     produto1 = () => {
         return './img/bolt-shirt-1200x1500.jpg'
     }
     msgError = () => {
         return '[data-test="error"]'
+    }
+    contadorPrd = () => {
+        return '.fa-layers-counter'
+    }
+    btnCheckoutFinish = () => {
+        return '.btn_action';
+    }
+    nome = () => {
+        return '[data-test="firstName"]'
+    }
+    sobrenome = () => {
+        return '[data-test="lastName"]'
+    }
+    CEP = () => {
+        return '[data-test="postalCode"]'
+    }
+    btnContinue = () => {
+        return '.btn_primary'
+    }
+    qtdCarrinho = () => {
+        return '.summary_quantity'
     }
 }
 
